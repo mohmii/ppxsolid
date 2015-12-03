d4cd2f9a67ae57f3d40bc9f037ca4abcef597b62
mohmii <mohmii@users.noreply.github.com>
2015-12-03 15:49:47 +0900

    merge with hirai

--- cs_ppx/trunk/cs_ppx/control_pp_details.Designer.cs
+++ cs_ppx/trunk/cs_ppx/control_pp_details.Designer.cs
@@ -51,11 +51,9 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 242);
             this.label1.Location = new System.Drawing.Point(4, 279);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 13);
             this.label1.Size = new System.Drawing.Size(158, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Machining Plane Details";
@@ -73,13 +71,11 @@
             this.PlaneScore,
             this.AddRemark,
             this.ObjectPtr});
-            this.MP_details.Location = new System.Drawing.Point(3, 258);
             this.MP_details.Location = new System.Drawing.Point(4, 298);
             this.MP_details.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.MP_details.Name = "MP_details";
             this.MP_details.ReadOnly = true;
             this.MP_details.RowHeadersVisible = false;
-            this.MP_details.Size = new System.Drawing.Size(304, 253);
             this.MP_details.Size = new System.Drawing.Size(405, 292);
             this.MP_details.TabIndex = 2;
             this.MP_details.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.MP_details_CellContentClick);
@@ -91,7 +87,6 @@
             this.PlaneName.Name = "PlaneName";
             this.PlaneName.ReadOnly = true;
             this.PlaneName.ToolTipText = "The plane name";
-            this.PlaneName.Width = 60;
             this.PlaneName.Width = 68;
             // 
             // Distance
@@ -101,7 +96,6 @@
             this.Distance.Name = "Distance";
             this.Distance.ReadOnly = true;
             this.Distance.ToolTipText = "The distance from the virtual centroid";
-            this.Distance.Width = 65;
             this.Distance.Width = 79;
             // 
             // PlaneScore
@@ -111,7 +105,6 @@
             this.PlaneScore.Name = "PlaneScore";
             this.PlaneScore.ReadOnly = true;
             this.PlaneScore.ToolTipText = "The score based on the number of intersection with other planes";
-            this.PlaneScore.Width = 60;
             this.PlaneScore.Width = 70;
             // 
             // AddRemark
@@ -133,11 +126,9 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 514);
             this.label2.Location = new System.Drawing.Point(4, 593);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 13);
             this.label2.Size = new System.Drawing.Size(150, 15);
             this.label2.TabIndex = 3;
             this.label2.Text = "Machining Plan Details";
@@ -145,11 +136,9 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button1.Location = new System.Drawing.Point(3, 617);
             this.button1.Location = new System.Drawing.Point(4, 712);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 35);
             this.button1.Size = new System.Drawing.Size(115, 40);
             this.button1.TabIndex = 5;
             this.button1.Text = "Generate";
@@ -161,11 +150,9 @@
             this.MachiningTree.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.MachiningTree.Location = new System.Drawing.Point(3, 530);
             this.MachiningTree.Location = new System.Drawing.Point(4, 612);
             this.MachiningTree.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.MachiningTree.Name = "MachiningTree";
-            this.MachiningTree.Size = new System.Drawing.Size(304, 81);
             this.MachiningTree.Size = new System.Drawing.Size(404, 93);
             this.MachiningTree.TabIndex = 6;
             this.MachiningTree.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
@@ -175,7 +162,6 @@
             // 
             this.log_messages.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.log_messages.Location = new System.Drawing.Point(3, 21);
             this.log_messages.Location = new System.Drawing.Point(4, 24);
             this.log_messages.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.log_messages.Multiline = true;
@@ -186,11 +172,9 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 5);
             this.label3.Location = new System.Drawing.Point(4, 6);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(66, 13);
             this.label3.Size = new System.Drawing.Size(87, 15);
             this.label3.TabIndex = 8;
             this.label3.Text = "Process Log";
@@ -198,11 +182,9 @@
             // clear_all
             // 
             this.clear_all.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.clear_all.Location = new System.Drawing.Point(211, 617);
             this.clear_all.Location = new System.Drawing.Point(281, 712);
             this.clear_all.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.clear_all.Name = "clear_all";
-            this.clear_all.Size = new System.Drawing.Size(96, 35);
             this.clear_all.Size = new System.Drawing.Size(115, 40);
             this.clear_all.TabIndex = 9;
             this.clear_all.Text = "Clear All";
@@ -222,7 +204,6 @@
             // 
             // control_pp_details
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.button2);
@@ -236,7 +217,6 @@
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "control_pp_details";
-            this.Size = new System.Drawing.Size(310, 655);
             this.Size = new System.Drawing.Size(413, 756);
             this.Load += new System.EventHandler(this.UserControl1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.MP_details)).EndInit();
